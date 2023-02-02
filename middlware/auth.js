94b34c948f9acda62d7be5ca96a704c6874aa214
kunal <106508267+kunalvishawkarma@users.noreply.github.com>
2023-02-02 15:21:45 +0530

    Update auth.js

--- middlware/auth.js
+++ middlware/auth.js
@@ -3,23 +3,20 @@ const config = require("../config/config");
 
 
 const verifyToken = async(req,res,next) => {
-   
+     try {
     const token = req.body.token || req.query.token || req.headers["authorization"];
-
     if(!token){
-        res.status(200).send({success:false,msg:"token is required for authrization..."})
+        res.status(200).send({success:false,msg:"token is required for authrization..."});
+    }else{
+        const decode  =  jwt.verify(token,config.secret_jwt);
+        req.user = decode;
+        return next();
     }
-
-    try {
-
-      const decode  =  jwt.verify(token,config.secret_jwt);
-      req.user = decode;
-
-    } catch (error) {
-        res.status(401).send("invalid token");
-    }
-     return next();
+   } catch (error) {
+    res.status(401).send("invalid token");
+   }
 }; 
+
 
 
 module.exports = verifyToken
